00a7994cd16f2a7a1a8ba370da9e29bdf649f412
Sherlyn <sherlynpaterson@gmail.com>
2021-08-05 17:52:21 -0400

    Initial commit

---
+++ 100-Days-Of-Code/src/com/sherlynpaterson/DayOne/DayOne.java
@@ -0,0 +1,9 @@
+package com.sherlynpaterson.DayOne;
+
+public class DayOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+// this allows for a single line comment
+    /* this allows for multi-line comments  */
+}
